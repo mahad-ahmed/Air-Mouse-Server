4ed4f86905c7d41dc46b5c40bda837acd71a3f09
mahad-ahmed <Gpassword_1>
2020-08-19 01:43:30 +0500

    Commit last changes

---
+++ Air Mouse Server/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Air Mouse Server.rc
+//
+#define IDI_SMALL                       102
+#define IDS_APP_TITLE                   103
+#define IDI_AIR_MOUSE_SERVER            108
+#define IDC_AIR_MOUSE_SERVER            109
+#define IDR_AIR_MOUSE_TRAY              110
+#define ID_FILE_MINIMIZETOTRAY          40006
+#define IDM_FILE_QUIT                   40007
+#define ID_HELP_CHECKFORUPDATEDVERSION  40010
+#define IDM_FILE_MINIMIZE_TO_TRAY       40011
+#define IDM_HELP_ANDROIDAPPLICATION     40014
+#define IDM_TRAY_OPEN                   40016
+#define IDM_TRAY_QUIT                   40018
+#define ID_REPAINT                      40013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40017
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
